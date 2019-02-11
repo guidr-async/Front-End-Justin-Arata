4e9743103227d3d9e5b3a486411fad7ae3a8574e
J A <jarata@gmail.com>
2019-02-11 10:14:11 -0800

    base Trip card

--- guidr/src/components/homepage/Trip.js
+++ guidr/src/components/homepage/Trip.js
@@ -1,21 +1,30 @@
 import React from 'react';
+
+import styled from 'styled-components'
+
+const TripCard = styled.div`
+  border: 1px solid black;
+`
+
+
 const Trip = props => {
     return (
-        <div>
+        <TripCard>
             <div>
-                {props.title}
+                <p>{props.trip.title}</p>
             </div>
             <div>
-                {props.location}
-                {props.adventure_type}
-                {props.duration}
-                {props.professional}
+                <p>Location: {props.trip.location}</p>
+                <p>Adventure Type: {props.trip.adventure_type}</p>
+                <p>Duration: {props.trip.duration}</p>
+                <p>Work or Pleasure? {props.trip.professional}</p>
             </div>
             <div>
-                {props.user_id}
-                {props.date}
+                {/*need to make this username string*/}
+                <p>{props.trip.user_id}</p>
+                <p>Trip Date: {props.trip.date}</p>
             </div>
-        </div>
+        </TripCard>
     )
 }
 export default Trip;
