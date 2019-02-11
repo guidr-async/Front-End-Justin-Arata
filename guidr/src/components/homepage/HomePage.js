07787673c30d9694c59d99923d401390c263e73c
J A <jarata@gmail.com>
2019-02-11 12:21:47 -0800

    plans for HomePage component

---
+++ guidr/src/components/homepage/HomePage.js
@@ -0,0 +1,13 @@
+// nav bar
+    // add a trip link
+    // profile link
+        // info editable
+        // user adventures displayed in trip card area
+    // home page
+    // logout link
+
+    // trip type filter link
+        // all, hiking, backpacking, rock climbing, cycling, scuba diving
+
+// list of trip cards displayed out
+
