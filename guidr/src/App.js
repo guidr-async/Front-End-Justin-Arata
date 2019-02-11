f70309ea1c2b869ca5411c3271eb4ec2c8ad3b82
J A <jarata@gmail.com>
2019-02-11 12:26:07 -0800

    refactoring App and HomePage

--- guidr/src/App.js
+++ guidr/src/App.js
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TripList from "./components/homepage/TripList";
-import axios from 'axios'
-import AddTripForm from "./components/addTrip/AddTripForm";
-import Profile from "./components/profile/Profile";
+import HomePage from "./components/homepage/HomePage";
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      trips: []
-    }
-  }
-
-  componentDidMount() {
-    axios
-        .get('https://guidr2.herokuapp.com/adventures')
-        .then(response => {
-          console.log(response)
-          this.setState({
-            trips: response.data
-          })
-        })
-        .catch(error => {
-          console.log(error)
-        })
-  }
-
   render() {
     return (
       <div className="App">
-        <TripList trips={this.state.trips} />
-        <AddTripForm />
-        <Profile/>
+        <HomePage />
       </div>
     );
   }
--- guidr/src/components/homepage/HomePage.js
+++ guidr/src/components/homepage/HomePage.js
@@ -11,3 +11,37 @@
 
 // list of trip cards displayed out
 
+import React, {Component} from 'react';
+import TripList from "./TripList";
+import axios from "axios";
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://guidr2.herokuapp.com/adventures')
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    trips: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <TripList />
+            </div>
+        );
+    }
+}
+
+export default HomePage;
